7f043f7be41d1d5477f9909a79446a23b88eaf30
Geoff Brousseau <geoff_brousseau@yahoo.com>
2020-11-20 08:48:19 -0800

    minor changes to styling and workOrder.js

--- src/components/WorkOrder.js
+++ src/components/WorkOrder.js
@@ -48,28 +48,28 @@ class WorkOrder extends Component {
             <h2>General</h2>
             <hr />
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Id:</div><div className="d-inline w-50 p-5">{data.id ? data.id : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Id:</div><div className="d-inline min-vw-50 p-5">{data.id ? data.id : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Root id:</div><div className="d-inline w-50 p-5">{data.rootId ? data.rootId : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Root id:</div><div className="d-inline min-vw-50 p-5">{data.rootId ? data.rootId : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Number:</div><div className="d-inline w-50 p-5">{data.number ? data.number : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Number:</div><div className="d-inline min-vw-50 p-5">{data.number ? data.number : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Title:</div><div className="d-inline w-50 p-5">{data.title ? data.title : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Title:</div><div className="d-inline min-vw-50 p-5">{data.title ? data.title : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Description:</div><div className="d-inline w-50 p-5">{data.description ? data.description : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Description:</div><div className="d-inline min-vw-50 p-5">{data.description ? data.description : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Priority:</div><div className="d-inline w-50 p-5">{data.priority ? data.priority : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Priority:</div><div className="d-inline min-vw-50 p-5">{data.priority ? data.priority : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Status:</div><div className="d-inline w-50 p-5">{data.status ? data.status : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Status:</div><div className="d-inline min-vw-50 p-5">{data.status ? data.status : "No data"}</div>
             </div>
             <div className="col-4 float-left">
-              <div className="w-50 d-inline font-weight-bold">Category:</div><div className="d-inline w-50 p-5">{data.category ? data.category : "No data"}</div>
+              <div className="min-vw-50 d-inline font-weight-bold">Category:</div><div className="d-inline min-vw-50 p-5">{data.category ? data.category : "No data"}</div>
             </div>
           </div>
           {data.schedule == null ?
@@ -78,19 +78,19 @@ class WorkOrder extends Component {
               <h2 className="my-3">Schedule</h2>
               <hr />
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Value:</div><div className="d-inline w-50 p-5">{data.schedule.value}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Value:</div><div className="d-inline min-vw-50 p-5">{data.schedule.value}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Interval:</div><div className="d-inline w-50 p-5">{data.schedule.interval}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Interval:</div><div className="d-inline min-vw-50 p-5">{data.schedule.interval}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Type:</div><div className="d-inline w-50 p-5">{data.schedule.type}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Type:</div><div className="d-inline min-vw-50 p-5">{data.schedule.type}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Days:</div><div className="d-inline w-50 p-5">{data.schedule.days}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Days:</div><div className="d-inline min-vw-50 p-5">{data.schedule.days}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Use last completed:</div><div className="d-inline w-50 p-5">{data.schedule.useLastCompleted}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Use last completed:</div><div className="d-inline min-vw-50 p-5">{data.schedule.useLastCompleted}</div>
               </div>
               <div className="col-4 float-left" />
             </div>
@@ -101,16 +101,16 @@ class WorkOrder extends Component {
               <h2 className="my-3">Created By</h2>
               <hr />
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Id:</div><div className="d-inline w-50 p-5">{data.createdBy.id ? data.createdBy.id : "No data"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Id:</div><div className="d-inline min-vw-50 p-5">{data.createdBy.id ? data.createdBy.id : "No data"}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">User name:</div><div className="d-inline w-50 p-5">{data.createdBy.username ? data.createdBy.username : "No data"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">User name:</div><div className="d-inline min-vw-50 p-5">{data.createdBy.username ? data.createdBy.username : "No data"}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">First name:</div><div className="d-inline w-50 p-5">{data.createdBy.firstName ? data.createdBy.firstName : "No data"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">First name:</div><div className="d-inline min-vw-50 p-5">{data.createdBy.firstName ? data.createdBy.firstName : "No data"}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Last name:</div><div className="d-inline w-50 p-5">{data.createdBy.lastName ? data.createdBy.lastName : "No data"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Last name:</div><div className="d-inline min-vw-50 p-5">{data.createdBy.lastName ? data.createdBy.lastName : "No data"}</div>
               </div>
             </div>
           }
@@ -120,13 +120,13 @@ class WorkOrder extends Component {
               <h2 className="my-3">Location</h2>
               <hr />
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">Id:</div><div className="d-inline w-50 p-5">{data.location.id ? data.location.id : "--"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Id:</div><div className="d-inline min-vw-50 p-5">{data.location[0].id ? data.location[0].id : "No data"}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">User name:</div><div className="d-inline w-50 p-5">{data.location.name ? data.location.name : "--"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Name:</div><div className="d-inline min-vw-50 p-5">{data.location[0].name ? data.location[0].name : "No data"}</div>
               </div>
               <div className="col-4 float-left">
-                <div className="w-50 d-inline font-weight-bold">First name:</div><div className="d-inline w-50 p-5">{data.location.address ? data.location.address : "--"}</div>
+                <div className="min-vw-50 d-inline font-weight-bold">Address:</div><div className="d-inline min-vw-50 p-5">{data.location[0].address ? data.location[0].address : "No data"}</div>
               </div>
             </div>
           }
